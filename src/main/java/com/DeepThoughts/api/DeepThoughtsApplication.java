f74fff4ab2013e263786d8caaa46f24b9124b11c
Daniel Clark <dclark253@outlook.com>
2023-04-05 10:35:09 -0700

    Initial commit

---
+++ src/main/java/com/DeepThoughts/api/DeepThoughtsApplication.java
@@ -0,0 +1,13 @@
+package com.DeepThoughts.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeepThoughtsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeepThoughtsApplication.class, args);
+	}
+
+}
